9aa207d067794be2d34b94c14dce5109dc79dcd5
Frank-Xianran-Liao <xianranl@uci.edu>
2021-07-15 00:47:53 +0800

    Updated on 7/15/2021
    The latest version.
    More reader-friendly.

--- crawler_cloudflare.py
+++ crawler_cloudflare.py
@@ -22,19 +22,20 @@ import pymongo
 #  - retry(): It is called at the end of the start_crawling() function to retry all previously failed URLs.
 #  - retry_url(url_retry, isExternal): It is responsible for the main retrying process.
 #  - handle_url(current_url, isExternal): Retrieving contents from the given URL.
-#  - store_crawled_data(url_response, id, slug, real_url): Storing the crawled data on Mongodb.
-#  - extract_next_urls(json_url): Taking in an URL which contains URL information of all the posts on that page in JSON format,
-#  return those URLs.
+#  - store_crawled_data(url_response, id, slug, real_url): Storing the refined crawled data on Mongodb. After storing,
+# it crawls the URLs that are included in the post contents and stores the crawled data on Mongodb.
+#  - extract_next_urls(json_url): Taking in one cloudflare community page URL which contains URL information of all the
+#  posts on that page in JSON format and returning those URLs.
 #  - external_crawl(included_url): Crawling URLs that are included in the posts.
 #  - retry_external(): Retrying failed external URLs.
 #  - store_external(url_response, included_url): Storing the crawled data on Mongodb.
 
 class Crawler_cloudflare:
     def __init__(self):
-        self.url_queue = deque([])
-        self.dir = "D:/Crawler_Project/Crawled_Data"
+        #self.url_queue = deque([])
+        #self.dir = "D:/Crawler_Project/Crawled_Data"
         self.pid = 0
-        self.doc_count = 0
+        #self.doc_count = 0
         self.crawled_count = 0
 
         self.crawl_interval = 0.35
@@ -46,16 +47,15 @@ class Crawler_cloudflare:
                             "Accept": "application/json"}
 
         # These proxies are found from the Internet. I will delete them if they are not appropriate.
-        # This version is temporarily not using proxies.
+        # This version is temporarily NOT using proxies.
 
-        self.proxies = [#{"http": "174.81.78.64:48678"},
-                        # {"http": "95.208.208.227:3128"},
-                        # {"http": "153.120.137.91:8080"},
-                        {"http": "183.47.138.80:8888"},
-                        {"http": "120.34.216.47:9999"},
-                        {"http": "125.78.226.217:8888"},
-                        {"http": "27.150.87.5:9999"}]
-
+        # self.proxies = [#{"http": "174.81.78.64:48678"},
+        #                 # {"http": "95.208.208.227:3128"},
+        #                 # {"http": "153.120.137.91:8080"},
+        #                 # {"http": "183.47.138.80:8888"},
+        #                 # {"http": "120.34.216.47:9999"},
+        #                 # {"http": "125.78.226.217:8888"},
+        #                 # {"http": "27.150.87.5:9999"}]]
 
         self.failed_queue = deque([])
         self.external_failed_queue = deque([])
@@ -98,18 +98,23 @@ class Crawler_cloudflare:
 
         self.retry()
 
+    def get_slug_id(self, url_retry):
+        id, slug, index, slashCount = "", "", -1, 0
+        while slashCount != 2:
+            if slashCount == 0 and url_retry[index] != "/":
+                id = url_retry[index] + id
+            if slashCount == 1 and url_retry[index] != "/":
+                slug = url_retry[index] + slug
+            if url_retry[index] == "/":
+                slashCount += 1
+            index -= 1
+
+        return id, slug
+
     def retry(self):
         while self.failed_queue:
             url_retry = self.failed_queue.popleft()
-            id, slug, index, slashCount = "", "", -1, 0
-            while slashCount != 2:
-                if slashCount == 0 and url_retry[index] != "/":
-                    id = url_retry[index] + id
-                if slashCount == 1 and url_retry[index] != "/":
-                    slug = url_retry[index] + slug
-                if url_retry[index] == "/":
-                   slashCount +=1
-                index -= 1
+            id, slug = self.get_slug_id(url_retry)
 
             print("PID {2} | Retrying URL: {0} Retried times: {1}".format(url_retry, self.failed_dict[url_retry], self.pid))
             self.failed_dict[url_retry] += 1
@@ -121,10 +126,22 @@ class Crawler_cloudflare:
 
             self.store_crawled_data(url_response, id, slug, url_retry)
 
-    def retry_url(self, url_retry, isExternal):
-        rint = random.randint(0, len(self.proxies) - 1)
-        proxy = self.proxies[rint]
+    def retry_url_helper(self, url_retry, isExternal):
+        if isExternal == 0:
+            if self.failed_dict[url_retry] < 3:
+                self.failed_queue.append(url_retry)
+            else:
+                print("PID {1} | Retry for URL: {0} FAILED.".format(url_retry, self.pid))
+        else:
+            if self.external_failed_dict[url_retry] < 3:
+                self.external_failed_queue.append(url_retry)
+            else:
+                print("PID {1} | Retry for External URL: {0} FAILED.".format(url_retry, self.pid))
+        return ""
 
+    def retry_url(self, url_retry, isExternal):
+        # rint = random.randint(0, len(self.proxies) - 1)
+        # proxy = self.proxies[rint]
         try:
             url_response = r.get(url_retry, timeout=20, headers=self.header)
             self.crawled_count += 1
@@ -132,53 +149,31 @@ class Crawler_cloudflare:
             return url_response
         except r.exceptions.RequestException as e:
             print("PID {2} | Failed to Retry: {0}, Error Message: {1}".format(url_retry, "Timeout", self.pid))
-            if isExternal == 0:
-                if self.failed_dict[url_retry] < 3:
-                    self.failed_queue.append(url_retry)
-                else:
-                    print("PID {1} | Retry for URL: {0} FAILED.".format(url_retry, self.pid))
-            else:
-                if self.external_failed_dict[url_retry] < 3:
-                    self.external_failed_queue.append(url_retry)
-                else:
-                    print("PID {1} | Retry for External URL: {0} FAILED.".format(url_retry, self.pid))
-            return ""
+            self.retry_url_helper(url_retry, isExternal)
         except:
             print("PID {2} | Failed to Retry: {0}, Error Message: {1}".format(url_retry, r.exceptions.ConnectionError.args, self.pid))
-            if isExternal == 0:
-                if self.failed_dict[url_retry] < 3:
-                    self.failed_queue.append(url_retry)
-                else:
-                    print("PID {1} | Retry for URL: {0} FAILED.".format(url_retry, self.pid))
-            else:
-                if self.external_failed_dict[url_retry] < 3:
-                    self.external_failed_queue.append(url_retry)
-                else:
-                    print("PID {1} | Retry for External URL: {0} FAILED.".format(url_retry, self.pid))
-            return ""
+            self.retry_url_helper(url_retry, isExternal)
+
+    def handle_url_helper(self, current_url, isExternal):
+        if isExternal == 0:
+            self.failed_queue.append(current_url)
+        else:
+            self.external_failed_queue.append(current_url)
+        return ""
 
     def handle_url(self, current_url, isExternal):
-        rint = random.randint(0, len(self.proxies) - 1)
-        proxy = self.proxies[rint]
-
+        # rint = random.randint(0, len(self.proxies) - 1)
+        # proxy = self.proxies[rint]
         try:
             url_response = r.get(current_url, timeout=15, headers=self.header)
             self.crawled_count += 1
             return url_response
         except r.exceptions.RequestException as e:
             print("PID {2} | Failed to Crawl: {0}, Error Message: {1}".format(current_url, "Timeout", self.pid))
-            if isExternal == 0:
-                self.failed_queue.append(current_url)
-            else:
-                self.external_failed_queue.append(current_url)
-            return ""
+            self.handle_url_helper(current_url, isExternal)
         except:
             print("PID {2} | Failed to Crawl: {0}, Error Message: {1}".format(current_url, r.exceptions.ConnectionError.args, self.pid))
-            if isExternal == 0:
-                self.failed_queue.append(current_url)
-            else:
-                self.external_failed_queue.append(current_url)
-            return ""
+            self.handle_url_helper(current_url, isExternal)
 
     def store_crawled_data(self, url_response, id, slug, real_url):
         # The commented code was used for storing data on the disk files
@@ -186,7 +181,7 @@ class Crawler_cloudflare:
         # if self.doc_count == 0 and "Page" + self.json_page_num not in os.listdir(self.dir):
         #     os.makedirs(self.dir + "/Page" + self.json_page_num)
 
-        current_dir = self.dir + "/Page" + self.json_page_num + "/" + slug + str(id) + ".txt"
+        # current_dir = self.dir + "/Page" + self.json_page_num + "/" + slug + str(id) + ".txt"
         # file = open(current_dir, "w", encoding="utf-8")
         # file.write(url_response.text)
         # file.close()
@@ -211,9 +206,8 @@ class Crawler_cloudflare:
         self.retry_external()
 
     def extract_next_urls(self, json_url):
-        rint = random.randint(0, len(self.proxies) - 1)
-        proxy = self.proxies[rint]
-
+        # rint = random.randint(0, len(self.proxies) - 1)
+        # proxy = self.proxies[rint]
         try:
             url_response = r.get(json_url, timeout=15, headers=self.json_header)
         except:
@@ -237,7 +231,7 @@ class Crawler_cloudflare:
         for ending in bList:
             if ending in included_url:
                 return
-        if "http" not in included_url or self.already_met[included_url] == 1:
+        if self.already_met[included_url] == 1:
             return
         self.already_met[included_url] = 1
 
@@ -273,8 +267,7 @@ class Crawler_cloudflare:
 
         insert_to_db = {"title": title,
                         "url": included_url,
-                        "content": url_response.text,
-                        }
+                        "content": url_response.text}
         self.collection.insert_one(insert_to_db)
 
 # A simple refiner that retrieves the post contents from the HTML file.
@@ -284,6 +277,9 @@ class refiner_cloudflare:
         self.cur_url = cur_url
         self.post_content = ""
         self.included_url = []
+        self.disallow = ["/admin/", "/auth/", "/email/", "/session",
+                         "/user-api-key", "/badges", "/u/", "/my", "/search",
+                         "/g", ".rss"]
 
     def refine(self):
         soup1 = BeautifulSoup(self.response.text, "lxml")
@@ -299,8 +295,11 @@ class refiner_cloudflare:
             new_url = anchor.get("href")
             abs_url = parse.urljoin(self.cur_url, new_url)
 
-            if "/u/" in abs_url:
-                continue
+            for disallow_element in self.disallow:
+                if disallow_element in abs_url:
+                    continue
+                if "http" not in abs_url:
+                    continue
             self.included_url.append(abs_url)
 
 
