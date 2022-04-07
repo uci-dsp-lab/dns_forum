d902666e4afe5bf373767821cc077e411b8417ab
Frank-Xianran-Liao <xianranl@uci.edu>
2022-04-07 15:30:50 -0700

    Update pymongo_queries.py

--- pymongo_queries.py
+++ pymongo_queries.py
@@ -14,6 +14,12 @@ db = client.cloudflare_crawled_data
 # setting the page range we want to check:
 startPage, endPage = 100, 200
 
+# To search all the pages for forums:
+# Cloudflare: 100, 1354
+# OpenDNS: 1, 19
+# Spiceworks: 1, 79
+# Comodo: 1, 10
+
 # go through those pages; each page is a collection
 for i in range(startPage, endPage + 1):
     # concatenating to get the collection name
@@ -28,3 +34,9 @@ for i in range(startPage, endPage + 1):
         postUrl = row["url"]
         print(postUrl)
         # please notice that the data structure of entry "replies" is a nested dictionary
+
+        # for label matching, search if a certain label is in row["labels"].
+        # for example, search if IPv4 exists in the post's labels:
+        if "IPv4" in row["labels"]:
+            # If there is IPv4 in its labels, print out the post's mongodb id
+            print(row["_id"])
